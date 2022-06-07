9575b0ae06a24f7541282fb0ea0880e74b38deeb
Stefan Muenzel <source@s.muenzel.net>
2022-06-07 11:46:39 +0800

    Splut lambda ml/mli/intf

--- ocamlc/lambda/lambda.ml
+++ ocamlc/lambda/lambda.ml
@@ -15,179 +15,8 @@
 
 open Misc
 open Asttypes
-
-type compile_time_constant =
-  | Big_endian
-  | Word_size
-  | Int_size
-  | Max_wosize
-  | Ostype_unix
-  | Ostype_win32
-  | Ostype_cygwin
-  | Backend_type
-
-type immediate_or_pointer =
-  | Immediate
-  | Pointer
-
-type initialization_or_assignment =
-  | Assignment
-  | Heap_initialization
-  | Root_initialization
-
-type is_safe =
-  | Safe
-  | Unsafe
-
-type primitive =
-  | Pbytes_to_string
-  | Pbytes_of_string
-  | Pignore
-    (* Globals *)
-  | Pgetglobal of Ident.t
-  | Psetglobal of Ident.t
-  (* Operations on heap blocks *)
-  | Pmakeblock of int * mutable_flag * block_shape
-  | Pfield of int * immediate_or_pointer * mutable_flag
-  | Pfield_computed
-  | Psetfield of int * immediate_or_pointer * initialization_or_assignment
-  | Psetfield_computed of immediate_or_pointer * initialization_or_assignment
-  | Pfloatfield of int
-  | Psetfloatfield of int * initialization_or_assignment
-  | Pduprecord of Types.record_representation * int
-  (* Context switches *)
-  | Prunstack
-  | Pperform
-  | Presume
-  | Preperform
-  (* External call *)
-  | Pccall of Primitive.description
-  (* Exceptions *)
-  | Praise of raise_kind
-  (* Boolean operations *)
-  | Psequand | Psequor | Pnot
-  (* Integer operations *)
-  | Pnegint | Paddint | Psubint | Pmulint
-  | Pdivint of is_safe | Pmodint of is_safe
-  | Pandint | Porint | Pxorint
-  | Plslint | Plsrint | Pasrint
-  | Pintcomp of integer_comparison
-  | Pcompare_ints | Pcompare_floats | Pcompare_bints of boxed_integer
-  | Poffsetint of int
-  | Poffsetref of int
-  (* Float operations *)
-  | Pintoffloat | Pfloatofint
-  | Pnegfloat | Pabsfloat
-  | Paddfloat | Psubfloat | Pmulfloat | Pdivfloat
-  | Pfloatcomp of float_comparison
-  (* String operations *)
-  | Pstringlength | Pstringrefu  | Pstringrefs
-  | Pbyteslength | Pbytesrefu | Pbytessetu | Pbytesrefs | Pbytessets
-  (* Array operations *)
-  | Pmakearray of array_kind * mutable_flag
-  | Pduparray of array_kind * mutable_flag
-  | Parraylength of array_kind
-  | Parrayrefu of array_kind
-  | Parraysetu of array_kind
-  | Parrayrefs of array_kind
-  | Parraysets of array_kind
-  (* Test if the argument is a block or an immediate integer *)
-  | Pisint
-  (* Test if the (integer) argument is outside an interval *)
-  | Pisout
-  (* Operations on boxed integers (Nativeint.t, Int32.t, Int64.t) *)
-  | Pbintofint of boxed_integer
-  | Pintofbint of boxed_integer
-  | Pcvtbint of boxed_integer (*source*) * boxed_integer (*destination*)
-  | Pnegbint of boxed_integer
-  | Paddbint of boxed_integer
-  | Psubbint of boxed_integer
-  | Pmulbint of boxed_integer
-  | Pdivbint of { size : boxed_integer; is_safe : is_safe }
-  | Pmodbint of { size : boxed_integer; is_safe : is_safe }
-  | Pandbint of boxed_integer
-  | Porbint of boxed_integer
-  | Pxorbint of boxed_integer
-  | Plslbint of boxed_integer
-  | Plsrbint of boxed_integer
-  | Pasrbint of boxed_integer
-  | Pbintcomp of boxed_integer * integer_comparison
-  (* Operations on Bigarrays: (unsafe, #dimensions, kind, layout) *)
-  | Pbigarrayref of bool * int * bigarray_kind * bigarray_layout
-  | Pbigarrayset of bool * int * bigarray_kind * bigarray_layout
-  (* size of the nth dimension of a Bigarray *)
-  | Pbigarraydim of int
-  (* load/set 16,32,64 bits from a string: (unsafe)*)
-  | Pstring_load_16 of bool
-  | Pstring_load_32 of bool
-  | Pstring_load_64 of bool
-  | Pbytes_load_16 of bool
-  | Pbytes_load_32 of bool
-  | Pbytes_load_64 of bool
-  | Pbytes_set_16 of bool
-  | Pbytes_set_32 of bool
-  | Pbytes_set_64 of bool
-  (* load/set 16,32,64 bits from a
-     (char, int8_unsigned_elt, c_layout) Bigarray.Array1.t : (unsafe) *)
-  | Pbigstring_load_16 of bool
-  | Pbigstring_load_32 of bool
-  | Pbigstring_load_64 of bool
-  | Pbigstring_set_16 of bool
-  | Pbigstring_set_32 of bool
-  | Pbigstring_set_64 of bool
-  (* Compile time constants *)
-  | Pctconst of compile_time_constant
-  (* byte swap *)
-  | Pbswap16
-  | Pbbswap of boxed_integer
-  (* Integer to external pointer *)
-  | Pint_as_pointer
-  (* Atomic operations *)
-  | Patomic_load of {immediate_or_pointer : immediate_or_pointer}
-  | Patomic_exchange
-  | Patomic_cas
-  | Patomic_fetch_add
-  (* Inhibition of optimisation *)
-  | Popaque
-  (* Fetching domain-local state *)
-  | Pdls_get
-
-and integer_comparison =
-    Ceq | Cne | Clt | Cgt | Cle | Cge
-
-and float_comparison =
-    CFeq | CFneq | CFlt | CFnlt | CFgt | CFngt | CFle | CFnle | CFge | CFnge
-
-and value_kind =
-    Pgenval | Pfloatval | Pboxedintval of boxed_integer | Pintval
-
-and block_shape =
-  value_kind list option
-
-and array_kind =
-    Pgenarray | Paddrarray | Pintarray | Pfloatarray
-
-and boxed_integer = Primitive.boxed_integer =
-    Pnativeint | Pint32 | Pint64
-
-and bigarray_kind =
-    Pbigarray_unknown
-  | Pbigarray_float32 | Pbigarray_float64
-  | Pbigarray_sint8 | Pbigarray_uint8
-  | Pbigarray_sint16 | Pbigarray_uint16
-  | Pbigarray_int32 | Pbigarray_int64
-  | Pbigarray_caml_int | Pbigarray_native_int
-  | Pbigarray_complex32 | Pbigarray_complex64
-
-and bigarray_layout =
-    Pbigarray_unknown_layout
-  | Pbigarray_c_layout
-  | Pbigarray_fortran_layout
-
-and raise_kind =
-  | Raise_regular
-  | Raise_reraise
-  | Raise_notrace
+    
+include Lambda_intf.T
 
 let equal_boxed_integer = Primitive.equal_boxed_integer
 
@@ -205,26 +34,6 @@ let equal_value_kind x y =
   | Pintval, Pintval -> true
   | (Pgenval | Pfloatval | Pboxedintval _ | Pintval), _ -> false
 
-
-type structured_constant =
-    Const_base of constant
-  | Const_block of int * structured_constant list
-  | Const_float_array of string list
-  | Const_immstring of string
-
-type tailcall_attribute =
-  | Tailcall_expectation of bool
-    (* [@tailcall] and [@tailcall true] have [true],
-       [@tailcall false] has [false] *)
-  | Default_tailcall (* no [@tailcall] attribute *)
-
-type inline_attribute =
-  | Always_inline (* [@inline] or [@inline always] *)
-  | Never_inline (* [@inline never] *)
-  | Hint_inline (* [@inlined hint] attribute *)
-  | Unroll of int (* [@unroll x] *)
-  | Default_inline (* no [@inline] attribute *)
-
 let equal_inline_attribute x y =
   match x, y with
   | Always_inline, Always_inline
@@ -239,11 +48,6 @@ let equal_inline_attribute x y =
     | Hint_inline | Unroll _ | Default_inline), _ ->
     false
 
-type specialise_attribute =
-  | Always_specialise (* [@specialise] or [@specialise always] *)
-  | Never_specialise (* [@specialise never] *)
-  | Default_specialise (* no [@specialise] attribute *)
-
 let equal_specialise_attribute x y =
   match x, y with
   | Always_specialise, Always_specialise
@@ -253,108 +57,12 @@ let equal_specialise_attribute x y =
   | (Always_specialise | Never_specialise | Default_specialise), _ ->
     false
 
-type local_attribute =
-  | Always_local (* [@local] or [@local always] *)
-  | Never_local (* [@local never] *)
-  | Default_local (* [@local maybe] or no [@local] attribute *)
-
-type poll_attribute =
-  | Error_poll (* [@poll error] *)
-  | Default_poll (* no [@poll] attribute *)
-
-type function_kind = Curried | Tupled
-
-type let_kind = Strict | Alias | StrictOpt
-
-type meth_kind = Self | Public | Cached
-
 let equal_meth_kind x y =
   match x, y with
   | Self, Self -> true
   | Public, Public -> true
   | Cached, Cached -> true
   | (Self | Public | Cached), _ -> false
-
-type shared_code = (int * int) list
-
-type function_attribute = {
-  inline : inline_attribute;
-  specialise : specialise_attribute;
-  local: local_attribute;
-  poll: poll_attribute;
-  is_a_functor: bool;
-  stub: bool;
-  tmc_candidate: bool;
-}
-
-type scoped_location = Debuginfo.Scoped_location.t
-
-type lambda =
-    Lvar of Ident.t
-  | Lmutvar of Ident.t
-  | Lconst of structured_constant
-  | Lapply of lambda_apply
-  | Lfunction of lfunction
-  | Llet of let_kind * value_kind * Ident.t * lambda * lambda
-  | Lmutlet of value_kind * Ident.t * lambda * lambda
-  | Lletrec of (Ident.t * lambda) list * lambda
-  | Lprim of primitive * lambda list * scoped_location
-  | Lswitch of lambda * lambda_switch * scoped_location
-  | Lstringswitch of
-      lambda * (string * lambda) list * lambda option * scoped_location
-  | Lstaticraise of int * lambda list
-  | Lstaticcatch of lambda * (int * (Ident.t * value_kind) list) * lambda
-  | Ltrywith of lambda * Ident.t * lambda
-  | Lifthenelse of lambda * lambda * lambda
-  | Lsequence of lambda * lambda
-  | Lwhile of lambda * lambda
-  | Lfor of Ident.t * lambda * lambda * direction_flag * lambda
-  | Lassign of Ident.t * lambda
-  | Lsend of meth_kind * lambda * lambda * lambda list * scoped_location
-  | Levent of lambda * lambda_event
-  | Lifused of Ident.t * lambda
-
-and lfunction =
-  { kind: function_kind;
-    params: (Ident.t * value_kind) list;
-    return: value_kind;
-    body: lambda;
-    attr: function_attribute; (* specified with [@inline] attribute *)
-    loc: scoped_location; }
-
-and lambda_apply =
-  { ap_func : lambda;
-    ap_args : lambda list;
-    ap_loc : scoped_location;
-    ap_tailcall : tailcall_attribute;
-    ap_inlined : inline_attribute;
-    ap_specialised : specialise_attribute; }
-
-and lambda_switch =
-  { sw_numconsts: int;
-    sw_consts: (int * lambda) list;
-    sw_numblocks: int;
-    sw_blocks: (int * lambda) list;
-    sw_failaction : lambda option}
-
-and lambda_event =
-  { lev_loc: scoped_location;
-    lev_kind: lambda_event_kind;
-    lev_repr: int ref option;
-    lev_env: Env.t }
-
-and lambda_event_kind =
-    Lev_before
-  | Lev_after of Types.type_expr
-  | Lev_function
-  | Lev_pseudo
-  | Lev_module_definition of Ident.t
-
-type program =
-  { module_ident : Ident.t;
-    main_module_block_size : int;
-    required_globals : Ident.Set.t;
-    code : lambda }
 
 let const_int n = Const_base (Const_int n)
 
---
+++ ocamlc/lambda/lambda.mli
@@ -0,0 +1,1 @@
+include Lambda_intf.Lambda
