0911f5cf3ad8399f7c408400b00700de6f05fd69
Stefan Muenzel <source@s.muenzel.net>
2022-08-20 15:43:53 +0200

    Export more

--- lib/ocamlc_kit.ml
+++ lib/ocamlc_kit.ml
@@ -1,4 +1,6 @@
 
 module Asmcomp_platform = Asmcomp_platform
 module Asmcomp_platform_shared = Asmcomp_platform_shared
+module Backend_var = Backend_var
+module Cmm = Cmm
 module Debuginfo = Debuginfo
